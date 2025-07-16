9cfdfe0012424db8a90413526e09c074b8cc9acc
Surya Teja Pidakala <111381219+im-suryateja@users.noreply.github.com>
2025-07-16 16:15:39 -0400

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,19 @@
+USE master;
+GO
+
+--CREATE Warehouse
+CREATE DATABASE DataWarehouse1;
+GO
+
+
+USE DataWarehouse1;
+GO
+
+
+--CREATE SCHEMA
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
